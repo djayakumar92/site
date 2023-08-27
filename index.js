4a380d718722da298d82f2beb423fc707d618392
djayakumar92 <jayakumarksrct@gmail.com>
2023-08-27 08:52:59 +0530

    files updated

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-var data = [{ ID: 1, CustomerID: 'VINET', Freight: 32.38, OrderDate: new Date(8364186e5) },
-            { ID: 2, CustomerID: 'TOMSP', Freight: 11.61, OrderDate: new Date(836505e6) },
-            { ID: 3, CustomerID: 'HANAR', Freight: 65.83, OrderDate: new Date(8367642e5) },
-            { ID: 4, CustomerID: 'VICTE', Freight: 41.34, OrderDate: new Date(8368506e5) },
-           ];
-
-
-
+var data = [{ ID: 1, Name: 20201117, Date: new Date("2020/11/17"), Author: "" },
+{ ID: 2, Name: 20201203, Date: new Date("2020/12/03"), Author: "" },
+{ ID: 3, Name: 20210311, Date: new Date("2021/03/11"), Author: "" },
+{ ID: 4, Name: 20220314, Date: new Date("2022/03/14"), Author: "" },
+{ ID: 5, Name: 20220327, Date: new Date("2022/03/27"), Author: "" },
+];
 
 ej.grids.Grid.Inject(ej.grids.Page);
 var grid = new ej.grids.Grid({
     dataSource: data,
+    width:'100%',
+    height:'100%',
+    autoFit: true,
+    allowResizing: true,
     columns: [
-                { field: 'ID', headerText: 'ID', textAlign: 'Right', width: 100, type: 'number' },
                 {
-                    field: 'CustomerID', headerText: 'Quote', textAlign: 'Center', template:'#templateImg'
+                    field: 'Name', headerText: 'வாக்கியம்', template:'#templateImg'
                 },
-                
+                {
+                    field: 'Date', headerText: 'தேதி', format: 'dd/MM/yyy', width: 150,
+                },    
     ],
-    
-  
     });
 
 grid.appendTo('#Grid');
